d20e19176ea117d36fe9da1c2cf74c916907c3e4
alijafarkamal <ali.jafar.fast@gmail.com>
2024-04-29 19:49:44 +0500

    Pull

---
+++ first.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//#include<fstream>
+//#include<sstream>
+//#include<string>
+//#include<cstring>
+//#include<cctype>
+//#include<conio.h>
+//#include<iomanip>
+//
+//using namespace std;
+//int main()
+//{
+//	ofstream file("attendence.csv");
+//	file << "1001,";
+//	file << "ali,";
+//	string mark = "p a p p p p a p p\n p p p";
+//	file << mark;
+//	file.close();
+//	system("pause");
+//	return 0;
+//}
